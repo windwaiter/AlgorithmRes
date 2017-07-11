9cda0187eef27a8c12a3253ed39909ddab4bec7f
windwaiter <498505655@qq.com>
2017-07-11 17:08:38 +0800

    浪费排序

---
+++ SortBigGiftBag/src/WasteSort.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Ty
+ *
+ *不知道什么排序算法，时间复杂度O（n2），空间复杂度O（n2）
+ */
+public class WasteSort {
+	public void wasterSort(int[] arry){
+		for(int i=0;i<arry.length;i++){
+			for(int j=i;j<arry.length;j++){
+				if(arry[i]>arry[j]){
+					int location=arry[i];
+					arry[i]=arry[j];
+					arry[j]=location;
+				}
+			}
+		}
+	}
+}
