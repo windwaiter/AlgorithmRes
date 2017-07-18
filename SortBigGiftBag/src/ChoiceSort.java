6836180273d6bf69951dd1b33d08b8c52c5ae138
windwaiter <498505655@qq.com>
2017-07-18 14:57:45 +0800

    选择和浪费属于同一个排序

--- SortBigGiftBag/src/ChoiceSort.java
+++ SortBigGiftBag/src/ChoiceSort.java
@@ -1,4 +1,20 @@
 
+/**
+ * 
+ * @author Ty
+ *原来之前写出来的浪费排序就是选择排序
+ */
 public class ChoiceSort {
+	public void wasterSort(int[] arry){
+		for(int i=0;i<arry.length;i++){
+			for(int j=i;j<arry.length;j++){
+				if(arry[i]>arry[j]){
+					int location=arry[i];
+					arry[i]=arry[j];
+					arry[j]=location;
+				}
+			}
+		}
+	}
 
 }
