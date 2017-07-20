303926bd9efcf882488c1a7bbe4c5a1006e8892a
windwaiter <498505655@qq.com>
2017-07-20 11:20:32 +0800

    这个代码写的真好

--- SortBigGiftBag/src/排序算法大礼包/InsertSort.java
+++ SortBigGiftBag/src/排序算法大礼包/InsertSort.java
@@ -6,26 +6,16 @@ package 排序算法大礼包;
  *插入排序时间复杂度为O（n2）
  */
 public class InsertSort {
-	public void insertSort(int[] a){
-		int temp=0;
-		for(int i=0;i<a.length;i++){
-			for(int j=temp;j>0;j++){
-				if(a[temp]<a[i+1]){
-					temp=i+1;
-				}else{
-					int location=a[j];
-					a[j]=a[i+1];
-					a[i+1]=location;
-				}
-			}
-		}
-	}
-	public static void main(String[] args){
-		int[] a=new int[]{5,2,1,4,6};
-		InsertSort insertSort=new InsertSort();
-		insertSort.insertSort(a);
-		for(int i:a){
-			System.out.println(i);
-		}
-	}
+	public int[] insertionSort(int[] A, int n) {
+        int i, j, temp;
+        for(i = 1; i < n; i++){
+            temp = A[i];
+            for(j = i; j > 0 && A[j - 1] > temp; j-- ){
+                A[j] = A[j - 1];
+            }
+            A[j] = temp;
+        }
+          
+        return A;
+    }
 }
