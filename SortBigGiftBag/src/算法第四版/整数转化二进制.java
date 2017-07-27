e878af2999e03d53459d919fc4841e0330e3fc8f
windwaiter <498505655@qq.com>
2017-07-27 09:48:26 +0800

    精简

--- SortBigGiftBag/src/算法第四版/整数转化二进制.java
+++ SortBigGiftBag/src/算法第四版/整数转化二进制.java
@@ -3,8 +3,8 @@ package 算法第四版;
 public class 整数转化二进制 {
 	public String changeBinary(int num){
 		String s="";
-		for(int n=num;n>0;n/=2){
-			s=(n%2)+s;
+		for(;num>0;num/=2){
+			s=(num%2)+s;
 		}
 		return s;
 	}
